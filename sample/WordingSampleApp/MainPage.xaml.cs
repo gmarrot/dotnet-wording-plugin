edab9e2297c873bdadf7a9fd7dfb79036f57aa9a
Gaëtan Marrot <gaetan.marrot@gmail.com>
2019-06-23 18:57:49 +0200

    Creation of a sample application to test the plugin

---
+++ sample/WordingSampleApp/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace WordingSampleApp {
+    public partial class MainPage : ContentPage {
+
+        public MainPage() {
+            InitializeComponent();
+        }
+
+    }
+}
